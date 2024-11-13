5adab4da81a474667eb3b1897666c6458dcf0c0d
Antonio Mereu <101745513+AntonioMereu@users.noreply.github.com>
2024-11-13 12:07:49 +0000

    add navbar

--- config/db.js
+++ config/db.js
@@ -5,12 +5,12 @@ const { Pool } = pkg;
 dotenv.config();
 
 const db = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    // ssl: true,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  // ssl: true,
 });
 
 export default db;
