1682e1882c882dfa1d63752f4429fba25c7ea53e
Antonio <antn.mereu@gmail.com>
2025-06-08 13:10:36 +0200

    postgresql db connection ok, added ssl

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import dotenv from "dotenv";
 import pkg from "pg";
 const { Pool } = pkg;
@@ -6,11 +7,15 @@ dotenv.config();
 
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: true, // forza l'uso del certificato
+    ca: fs.readFileSync("./certs/prod-ca-2021.crt").toString(),
+  }
   // host: process.env.DB_HOST,
   // user: process.env.DB_USER,
   // database: process.env.DB_NAME,
   // password: process.env.DB_PASS,
-  // port: process.env.DB_PORT,
+  // port: process.env.DB_PORT
 });
 
 export default db;
